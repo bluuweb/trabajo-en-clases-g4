d32b05af18c7c42d6b3f9ef5ca5718894dfa0268
bluuweb <contacto@bluuweb.cl>
2022-03-29 22:11:38 -0300

    register user parte 1

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,7 @@
+const bcrypt = require("bcryptjs");
+const { nanoid } = require("nanoid");
 const { getUserDB } = require("../database/db");
+const path = require("path");
 
 const getUsers = async (req, res) => {
     const respuesta = await getUserDB();
@@ -8,6 +11,55 @@ const getUsers = async (req, res) => {
     return res.json({ users: respuesta.users });
 };
 
+const createUser = async (req, res) => {
+    try {
+        // validaciones
+        if (
+            !req.body?.nombre ||
+            !req.body?.email ||
+            !req.body?.password ||
+            !req.files?.foto
+        ) {
+            throw new Error("Todos los campos son obligatorios");
+        }
+
+        const { nombre, email, password } = req.body;
+        if (!nombre.trim() || !email.trim() || !password.trim()) {
+            throw new Error("Algunos campos están vacios");
+        }
+
+        // validaciones de las fotos
+        const { foto } = req.files;
+        const mimeTypes = ["image/jpeg", "image/png"];
+        if (!mimeTypes.includes(foto.mimetype)) {
+            throw new Error("Solo archivos png o jpg");
+        }
+        if (foto.size > 5 * 1024 * 1024) {
+            throw new Error("Máximo 5MB");
+        }
+
+        const pathFoto = `${nanoid()}.${foto.mimetype.split("/")[1]}`;
+        console.log(password);
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        // guardar img
+        foto.mv(path.join(__dirname, "../public/avatars/", pathFoto), (err) => {
+            if (err) throw new Error("No se puede guardar la img");
+        });
+
+        return res.json(req.body);
+    } catch (error) {
+        // console.log(error);
+        return res.status(400).json({
+            ok: false,
+            msg: error.message,
+        });
+    }
+};
+
 module.exports = {
     getUsers,
+    createUser,
 };
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const { getUsers } = require("../controllers/user.controller");
+const expressFileUpload = require("express-fileupload");
+const { getUsers, createUser } = require("../controllers/user.controller");
 const router = express.Router();
 
+router.use(
+    expressFileUpload({
+        abortOnLimit: true,
+        // limits: { fileSize: 5 * 1024 * 1024 },
+    })
+);
+
 router.get("/users", getUsers);
+router.post("/users", createUser);
 
 module.exports = router;
